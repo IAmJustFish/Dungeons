5776f0b818db023ceb22aa304863fe8d57aa394a
mmagistra <75617810+mmagistra@users.noreply.github.com>
2020-12-23 01:14:19 +0500

    2.1
    Update, if we wont game on open air (не подземелье)

--- drawer.py
+++ drawer.py
@@ -8,16 +8,22 @@ from ray_casting import ray_casting
 class Drawer:
     def __init__(self):
         self.textures = {'W1': pygame.image.load(os.path.join('data', 'W1.png')).convert_alpha(),
-                         'floor': 'None'}
+                         'floor': 'None',
+                         'sky': pygame.image.load(os.path.join('data', 'sky2.jpg')).convert_alpha()}
 
     def draw_all(self, screen, player, FPS=-1):
         screen.fill(BLACK)
-        self.draw_ground_and_sky(screen)
+
+        self.draw_ground_and_sky(screen, player)
+
         ray_casting(screen, player.pos, player.angle, self.textures)
+
         if DRAW_MINI_MAP:
             self.draw_mini_map(screen, player)
+
         if FPS >= 0 and SHOW_FPS:
             self.draw_FPS(screen, FPS)
+
         if SHOW_CROSSHAIR:
             self.draw_crosshair(screen)
 
@@ -49,8 +55,11 @@ class Drawer:
 
         screen.blit(mini_map, (WIDTH - WIDTH / MINI_MAP_SCALE, 0))
 
-    def draw_ground_and_sky(self, screen):
-        pygame.draw.rect(screen, BLACK, (0, 0, WIDTH, HALF_HEIGHT))
+    def draw_ground_and_sky(self, screen, player):
+        sky_offset = -5 * math.degrees(player.angle) % WIDTH
+        screen.blit(self.textures['sky'], (sky_offset, 0))
+        screen.blit(self.textures['sky'], (sky_offset - WIDTH, 0))
+        screen.blit(self.textures['sky'], (sky_offset + WIDTH, 0))
         pygame.draw.rect(screen, DARKGRAY, (0, HALF_HEIGHT, WIDTH, HALF_HEIGHT))
 
     def draw_crosshair(self, screen):
